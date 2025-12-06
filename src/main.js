b086cb41d8386329b95bb9a4361f576bee65c935
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2025-12-06 08:09:00 +0600

    add showProduct function

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ let products = [];
 let recentlyViewed = [];
 let filteredProducts=[];
 
+let currentUser={
+    name:'',
+    email:'',
+    phone:'',
+    address:''
+}
+
 async function loadData() {
     try {
         const response = await fetch('/src/data.json');
@@ -93,12 +100,16 @@ function showCategory(categoryId) {
         const category = categories.find(cat => cat.id === categoryId);
         document.getElementById('category-title').textContent = category.name;
     }
+
+    populateFilters();
+    renderProducts();
+    showPage('category')
+
 }
-showCategory(categoryId);
 
 function populateFilters() {
     const brandFilter = document.getElementById("brand-filter");
-    const brand=[...new Set(filteredProducts.map(product=>product.brand))];
+    const brands=[...new Set(filteredProducts.map(product=>product.brand))];
 
     brandFilter.innerHTML=`<option>All Brands</option>`;
     brands.forEach(brand=>{
@@ -139,6 +150,7 @@ function applyFilters() {
         default:
             break;
     }
+    renderProducts(filtered);
 };
 
 function renderProducts(products=filteredProducts) {
@@ -151,6 +163,101 @@ function renderProducts(products=filteredProducts) {
     products.forEach(product=>{
         const productCard=document.createElement("div");
         productCard.className="product-card";
-        productCard.onclick=()=>showProduct;
+        productCard.onclick=()=>showProduct(product.id);
+
+        productCard.innerHTML=`
+            <img src="${product.image}" alt="${product.name}"/>
+            <div class="product-card-content">
+            <div class="product-brand">${product.brand}</div>
+            <h3>${product.name}</h3>
+            <div class="product-rating">
+                ${"*".repeat(Math.floor(product.rating))}${"*".repeat(5-Math.floor(product.rating))}
+                ${product.rating}
+            </div>
+                <div class="product-price">
+                    <span class="current-price">${product.price}</span>
+                    <span class="original-price">${product.originalPrice}</span>
+                    <span class="discount-price">${product.discount}% OFF</span>
+                </div>
+            </div>
+        `
+        productGrid.appendChild(productCard)
     })
 };
+
+function showProduct(productId) {
+    const product=product.find(item=>item.id===productId);
+    if (!product) return;
+
+    if (!recentlyViewed.includes(productId)) {
+        recentlyViewed.unshift(productId);
+        if (recentlyViewed.length>10) {
+            recentlyViewed.pop()
+        }
+        saveRecentlyViewed();
+    }
+
+    const productDetail=document.getElementById('product-details');
+    const deliveryDate=new Date();
+    deliveryDate.setDate(deliveryDate.getDate()+7);
+
+    productDetail.innerHTML=`
+        <div>
+      <img src="${product.image}" alt="${product.name}" srcset="" class="product-image">
+    </div>
+    <div class="product-info">
+      <h1>${product.name}</h1>
+      <div class="brand">${product.brand}</div>
+      <div class="product-rating">
+            ${"*".repeat(Math.floor(product.rating))}${"*".repeat(5-Math.floor(product.rating))}
+                ${product.rating}/5
+      </div>
+        <div class="product-price">
+                    <span class="current-price">${product.price}</span>
+                    <span class="original-price">${product.originalPrice}</span>
+                    <span class="discount-price">${product.discount}% OFF</span>
+        </div>
+        <div class="description">${product.description}</div>
+        <div>
+            ${product.colors.length>0 ?`
+                <div class="option-group">
+                <label>Color:</label>
+                <select id="selected-color">
+                    ${product.colors.map(color=>`<option value="${color}>${color}</option>`).join("")}
+                </select>
+                </div>
+                `:""}
+
+                ${product.sizes.length>0 ?`
+                <div class="option-group">
+                <label>Sizes:</label>
+                <select id="selected-sizes">
+                    ${product.sizes.map(size=>`<option value="${size}>${size}</option>`).join("")}
+                </select>
+                </div>
+                `:""}
+        </div>
+        <div class="address-section">
+        <h3>Delivery-Address</h3>
+        ${currentUser.address ? `
+            <p>${currentUser.address}</p>
+            <button class="btn-secondary" onclick="showPage("account")">Change Address</button>`:`
+            <p>No address added</p>
+            <button class="btn-secondary" onclick="showPage("account")">Add Address</button>
+            `}
+        </div>
+        <div class="delivery-info">
+            <h4>Delivery Information</h4>
+            <p>Delivery by ${deliveryDate.toLocalDateString()}</p>
+            <p>10 days return policy</p>
+            <p>Cash on delivery available</p>
+        </div>
+        <div class="product-actions">
+            <button class="btn-primary" onclick="addToCart(${product.id})">Add to cart</button>
+            <button class="btn-secondary" onclick="buyNow(${product.id})">Buy Now</button>
+        </div>
+    </div>
+    `;
+
+    showPage('product')
+}
